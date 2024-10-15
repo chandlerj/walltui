b1316965dc1656e73b2fbbf5f71717c545eac5bf
Chandler J <cjustice2000@gmail.com>
2024-10-14 20:42:07 -0600

    we have gotten nowhere

---
+++ README.md
@@ -0,0 +1,7 @@
+### desired features
+- [ ] config file to set wallpaper folder
+- [ ] display file names in scrollable list that takes up left 1/4 of terminal
+- [ ] quit with Q
+- [ ] display preview of image on right side
+- [ ] R to move selection to random image
+- [ ] command line argument to skip tui and just a random wallpaper. (`-r` perhaps?)
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,94 @@
-use viuer::{print_from_file, Config};
+use std::io;
 
-fn main() {
-    let conf = Config {
-        // Set offset.
-        x: 20,
-        y: 4,
-        // Set dimensions.
-        width: Some(80),
-        height: Some(25),
-        ..Default::default()
-    };
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 
-    // Starting from row 4 and column 20,
-    // display `img.jpg` with dimensions 80×25 (in terminal cells).
-    // Note that the actual resolution in the terminal will be 80×50.
-    print_from_file("/home/chandler/Pictures/papes/waffles.jpg", &conf).expect("Image printing failed.");
+use ratatui::{
+    buffer::Buffer,
+    layout::{Alignment, Rect},
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{
+        block::{Position, Title}, Block, List, ListState, Paragraph, Widget, 
+    },
+    DefaultTerminal, Frame,
+};
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    terminal.clear()?;
+    let app_result = App::default().run(&mut terminal);
+    ratatui::restore();
+    app_result
 }
+
+#[derive(Debug, Default)]
+pub struct App {
+    exit: bool,
+    images: Vec<String>
+}
+
+impl App {
+
+    pub fn run (&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+    
+    fn draw(&self, frame: &mut Frame){
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()>{
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => { 
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+ 
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Title::from(" Wallpaper TUI ".bold());
+        let commands = Title::from(Line::from(vec![
+                " Select Random: r ".blue().into(),
+                " Quit: q ".red(),
+                
+        ]));
+
+        let mut state = ListState::default();
+
+        let items = ["first", "second", "third", "fourth"]
+
+        let block = Block::bordered()
+            .title(title)
+            .title(commands
+                    .alignment(Alignment::Center)
+                    .position(Position::Bottom),
+            )
+            .border_set(border::THICK);
+        Paragraph::new("hello")
+            .block(block)
+            .render(area, buf);
+    }
+}
+   
